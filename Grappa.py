9310b82130fc346752454d1b5cbde74135a1b85d
zsemi02 <tozseko@gmail.com>
2023-03-10 14:26:49 +0100

    OR SQL operator for multi select input fields

--- Grappa.py
+++ Grappa.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 import json
 import sqlite3
-from sqlalchemy import create_engine, text, MetaData, Table, sql, exc, select
+from sqlalchemy import create_engine, text, MetaData, Table, sql, exc, select, or_
 from sqlalchemy.inspection import inspect
 import sys
 import argparse
@@ -31,14 +31,6 @@ def loadPluginConfig():
 
 PLUGIN_CONF = loadPluginConfig()
 
-types = {
-    "integer": "number",
-    "varchar": "string",
-    "timestamp": "time",
-    "INTEGER": "number",
-    "TIMESTAMP": "time",
-}
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -65,13 +57,7 @@ def query():
     return result
 
 
-def parseType(type):
-    return types[str(type)]
-
-
 def queryDb(request):
-    
-    
     response = []
     targetnum = 0
     engine = create_engine(PLUGIN_CONF["connection"]["address"])
@@ -93,14 +79,39 @@ def queryDb(request):
             targetnum += 1
     return response
 
-def addToStatement(stmt, payload, elem):
-    if elem.startswith(">") or elem.startswith("<") or elem.startswith("!"):
+def addToStatementOr(vals, payload, elem):
+    if elem.startswith(">") or elem.startswith("<"):
+        if elem[1] == "=":
+            vals.append(text("{} {} {}".format(payload, elem[:2], elem[2:])))
+        else:
+            vals.append(text("{} {} {}".format(payload, elem[0], elem[1:])))
+    elif elem.startswith("!="):
+        if type(elem) is int:
+            vals.append(text("{} {} {}".format(payload, elem[:2], elem[2:])))
+        elif type(elem) is str:
+            vals.append(text("{} {} '{}'".format(payload, elem[:2], elem[2:])))
+    else:
+        if type(elem) is int:
+            vals.append(text("{} = {}".format(payload, elem)))
+        elif type(elem) is str:
+            vals.append(text("{} = '{}'".format(payload, elem)))
+
+def addToStatement(stmt, payload, elem):                                                                                #TODO: for multiselect values add OR operator
+    if elem.startswith(">") or elem.startswith("<"):
         if elem[1] == "=":
             stmt = stmt.where(text("{} {} {}".format(payload, elem[:2], elem[2:])))
         else:
             stmt = stmt.where(text("{} {} {}".format(payload, elem[0], elem[1:])))
+    elif elem.startswith("!="):
+        if type(elem) is int:
+            stmt = stmt.where(text("{} {} {}".format(payload, elem[:2], elem[2:])))
+        elif type(elem) is str:
+            stmt = stmt.where(text("{} {} '{}'".format(payload, elem[:2], elem[2:])))
     else:
-        stmt = stmt.where(text("{} = {}".format(payload, elem)))
+        if type(elem) is int:
+            stmt = stmt.where(text("{} = {}".format(payload, elem)))
+        elif type(elem) is str:
+            stmt = stmt.where(text("{} = '{}'".format(payload, elem)))
     return stmt
 
 def addFilteringOptions(stmt, target):
@@ -109,11 +120,16 @@ def addFilteringOptions(stmt, target):
     for payload, val in target["payload"].items():
         if payload not in cols:
             continue
-        if type(val) == list:
+        if type(val) == list:               #TODO OR operators here
+            vals = []
             for elem in val:
-                stmt = addToStatement(stmt, payload, elem)
+                #stmt = addToStatement(stmt, payload, elem)
+                addToStatementOr(vals, payload, elem)
+            stmt = stmt.where(or_(*vals))
+
         else:
             stmt = addToStatement(stmt, payload, val)
+    print(stmt)
     return stmt
 
 
@@ -127,7 +143,10 @@ def queryDataFromDb(conn, target, request):
         #result = conn.execute(text("SELECT {} FROM {} WHERE :time BETWEEN strftime('%s', :from)*1000 AND strftime('%s', :to)*1000".format(getTableColumnNamesStr(target["target"]), target["target"])), {"from": request["range"]["from"], "to": request["range"]["to"], "time": getTable(target["target"])["timeColumn"]})
         result = conn.execute(stmt, {"to": request["range"]["to"], "from": request["range"]["from"]})
     else:
-        result = conn.execute(text("SELECT {} FROM {}".format(getTableColumnNamesStr(target["target"]),target["target"])))
+        stmt = select(text("{}".format(getTableColumnNamesStr(target["target"])))).select_from(text("{}".format(target["target"])))
+        stmt = addFilteringOptions(stmt, target)
+        #result = conn.execute(text("SELECT {} FROM {}".format(getTableColumnNamesStr(target["target"]),target["target"])))
+        result = conn.execute(stmt)
     for row in result:
         rowList = []
         for value in row:
@@ -156,10 +175,6 @@ def getMetrics(conn):
         cols = getTableColumns(row)
         for col in cols:
             payload = {"name": col["name"], "type": col["input"]}
-            #if col["input"] == "select" or col["input"] == "multi-select":
-                #result = conn.execute(text("SELECT DISTINCT {} FROM {}".format(col["name"], row)))
-                #payload["options"] = [{"value": option[col["name"]], "label": option[col["name"]]} for option in result.mappings()]
-                #payload["reloadMetric"] = True
             obj["payloads"].append(payload)
 
         
@@ -174,15 +189,11 @@ def loadMetricPayloadOptions(req, conn):
         return []
 
 def getTableNames():
-    #insp = inspect(engine)
-    #return insp.get_table_names()
     return [(table["name"]) for table in PLUGIN_CONF["database"]["tables"]]
 def getTableColumns(table):
-    #insp = inspect(engine)
-    #return insp.get_columns(table)
     return [(col) for col in getTable(table)["columns"]]
 
-def getTableColumnNamesStr(table, exceptions = None):
+def getTableColumnNamesStr(table, exceptions = []):
     cols = getTableColumns(table)
     tableStr = ""
     for col in cols:
