6b3ad47705e703c223909bd5e6943fcfbdc04970
zsemi02 <tozseko@gmail.com>
2023-04-03 11:06:13 +0200

    Added basic function for query

--- influxdb.py
+++ influxdb.py
@@ -2,6 +2,9 @@ from PluginBase import PluginBase
 import influxdb_client
 from flask import request
 import json
+import time
+import datetime
+from dateutil.tz import tzutc
 
 class Plugin(PluginBase):
     def __init__(self, CONFIG, PLUGIN_CONF, logger) -> None:
@@ -24,14 +27,14 @@ class Plugin(PluginBase):
             obj["payloads"] = []
 
             
-            measurements_obj = {"name": "measurement", "type": "multi-select", "placeholder": "Select a measurement", "reloadMetric": True}
+            measurements_obj = {"name": "_measurement", "type": "multi-select", "placeholder": "Select a measurement", "reloadMetric": True}
 
             obj["payloads"].append(measurements_obj)
 
-            if "metric" in request and "payload" in request and "measurement" in request["payload"]:
+            if "metric" in request and "payload" in request and "_measurement" in request["payload"]:
                 # add tags and fields
                 tags = []
-                for measurement in request["payload"]["measurement"]:
+                for measurement in request["payload"]["_measurement"]:
                     key_query = """
                     import "influxdata/influxdb/schema"
 
@@ -48,6 +51,16 @@ class Plugin(PluginBase):
                             tagobj = {"name": val["_value"], "type": "multi-select", "placeholder": "Select tag"}
                             obj["payloads"].append(tagobj)
 
+                    fieldQuery = """import "influxdata/influxdb/schema"
+
+                                    schema.measurementFieldKeys(bucket: "{}", measurement: "{}")""".format(bucket.name, measurement)
+                    result = self.client.query_api().query(fieldQuery)
+                    for val in result[0]:
+                        val = val.values
+                        tagobj = {"name": val["_value"], "type": "input", "placeholder": "Filter field"}
+                        obj["payloads"].append(tagobj)
+                    
+
             response.append(obj)
         return response
 
@@ -55,7 +68,7 @@ class Plugin(PluginBase):
 
     def loadMetricPayloadOptions(self, req):
         resp = []
-        if req["name"] == "measurement":
+        if req["name"] == "_measurement":
             query = f"""
                         import \"influxdata/influxdb/schema\"
 
@@ -71,9 +84,9 @@ class Plugin(PluginBase):
             return resp
 
         # Multiple measurments are possible, and they might have fields with the same name. Maybe querying the field values for all measurements and then merging them? 
-        if "metric" in req and "payload" in req and "measurement" in req["payload"]:
+        if "metric" in req and "payload" in req and "_measurement" in req["payload"]:
             values = []
-            for measurement in req["payload"]["measurement"]:
+            for measurement in req["payload"]["_measurement"]:
                 query = f"""import \"influxdata/influxdb/schema\"
 
                 schema.measurementTagValues(bucket: \"{req["metric"]}\", measurement: \"{measurement}\", tag: \"{req["name"]}\")"""
@@ -91,4 +104,45 @@ class Plugin(PluginBase):
 
 
     def queryDb(self, request):
-        return []
+        resp = []
+        targets = request["targets"]
+        for target in targets:
+            payload = target["payload"]
+            query = "from(bucket: \"{}\") |> range(start: {}, stop: {})".format(target["target"], request["range"]["from"], request["range"]["to"])
+            payloadQuery = " |> filter(fn: (r) => {})"
+            payloadQueryData = ""
+            for key in payload:
+                if type(payload[key]) == list:
+                    iterations = 0
+                    for val in payload[key]:
+                        payloadQueryData = payloadQueryData + "r.{} == \"{}\" or ".format(key, val)
+                        iterations += 1
+                    if iterations > 0:
+                        payloadQueryData = payloadQueryData[:-3] + "and "
+                else:
+                    payloadQueryData = payloadQueryData + "r.{} == \"{}\" and ".format(key, payload[key])
+            payloadQueryData = payloadQueryData[:-5]
+
+            if "_measurement" in payload and len(payload["_measurement"]) > 0:
+                queryToRun = query + payloadQuery.format(payloadQueryData)
+            else:
+                queryToRun = query
+
+            print(queryToRun)
+
+            results = self.client.query_api().query(queryToRun)
+            respTargets = {}
+            for result in results:
+                for val in result:
+                    print(val)
+                    print()
+                    val = val.values
+                    if val["_field"] not in respTargets:
+                        respTargets[val["_field"]] = []
+                    respTargets[val["_field"]].append([val["_value"], int(val["_time"].timestamp())*1000])
+            
+            for obj in respTargets:
+                resp.append({"target": obj, "datapoints": respTargets[obj]})
+            print(resp)
+
+        return resp
