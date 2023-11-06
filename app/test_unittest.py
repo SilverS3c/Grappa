5282b92552865952aeb98780addbbaf5605a38e1
SilverS3c <tozseko@gmail.com>
2023-11-06 15:35:43 +0100

    Correction to test file

--- app/test_unittest.py
+++ app/test_unittest.py
@@ -20,7 +20,7 @@ class TestGrappa(unittest.TestCase):
         self.assertEqual(self.sqlPlugin.getTableColumnNamesStr("ram_usage", "time"), "ram")
 
     def testQuery(self):
-        request = {"targets": [{"target": "ram_usage", "payload": {}}], "range": {"from": str((datetime.datetime.fromtimestamp(1699266108) - datetime.timedelta(hours=2)).isoformat()) + ".0Z", "to": str(datetime.datetime.fromtimestamp(1699266108).isoformat()) + ".0Z"}}
+        request = {"targets": [{"target": "ram_usage", "payload": {}}], "range": {"from": str((datetime.datetime.fromtimestamp(1699266108) - datetime.timedelta(hours=2)).isoformat()) + ".0Z", "to": str((datetime.datetime.fromtimestamp(1699266108) + datetime.timedelta(hours=2)).isoformat()) + ".0Z"}}
         self.assertEqual(self.sqlPlugin.queryDb(request), [{'target': 'ram_usage', 'datapoints': [[50.0, 1699266108000], [60.0, 1699266109000], [70.0, 1699266110000]]}])
 
 if __name__ == '__main__':
